805e63dbbe63e49f9f79c2baa41d0d5a0f0bf3a9
guojia1126 <176566340@qq.com>
2014-12-15 20:42:58 +0800

    112

---
+++ src/main/java/org/mybatis/spring/sample/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package org.mybatis.spring.sample.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface UserMapper {
+	@Select("SELECT * FROM users WHERE id = #{userId}")
+	User getUser(@Param("userId") String userId);
+}
